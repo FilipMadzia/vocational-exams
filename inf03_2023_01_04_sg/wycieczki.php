18fd8ade830a437b28a1bec770f66b245434d48c
Filip Madzia <filipmadzia33@gmail.com>
2024-11-23 20:50:21 +0100

    HTML + CSS

---
+++ inf03_2023_01_04_sg/wycieczki.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Wycieczki po Europie</title>
+    <link rel="stylesheet" href="styl4.css">
+</head>
+<body>
+    <header>
+        <h1>BIURO TURYSTYCZNE - Kasamba</h1>
+    </header>
+
+    <content>
+        <section id="blok-z-danymi">
+            <h3>Wycieczki, na które są wolne miejsca</h3>
+            <ul>
+                <!-- TODO -->
+            </ul>
+        </section>
+        <section id="blok-lewy">
+            <h2>Bestselery</h2>
+            <table>
+                <tr>
+                    <td>Wenecja</td>
+                    <td>kwiecień</td>
+                </tr>
+                <tr>
+                    <td>Londyn</td>
+                    <td>lipiec</td>
+                </tr>
+                <tr>
+                    <td>Rzym</td>
+                    <td>wrzesień</td>
+                </tr>
+            </table>
+        </section>
+        <section id="blok-srodkowy">
+            <h2>Nasze zdjęcia</h2>
+            <!-- TODO -->
+        </section>
+        <section id="blok-prawy">
+            <h2>Skontaktuj się</h2>
+            <a href="email:turysta@wycieczki.pl">napisz do nas</a>
+            <p>telefon: 111222333</p>
+        </section>
+    </content>
+
+    <footer>
+        <p>Stronę wykonał: Filip Madzia 23.11.2024</p>
+    </footer>
+</body>
+</html>
