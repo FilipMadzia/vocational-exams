2eddce2a5d38891e03615bf3badad26435b2095f
Filip Madzia <filipmadzia33@gmail.com>
2024-11-29 17:00:43 +0100

    Skrypty

--- inf03_2023_06_04_sg/biblioteka.php
+++ inf03_2023_06_04_sg/biblioteka.php
@@ -1,3 +1,7 @@
+<?php
+$polaczenie = mysqli_connect('localhost', 'root', '', 'biblioteka');
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -13,33 +17,61 @@
     <main>
         <section id="blok-lewy">
             <h3>Polecamy dzieła autorów</h3>
-            <!-- Skrypt 1 -->
+            <ol>
+                <!-- Skrypt 1 -->
+                <?php
+                $zapytanie = "SELECT autorzy.imie, autorzy.nazwisko FROM autorzy ORDER BY autorzy.nazwisko ASC;";
+
+                $wynik_zapytania = mysqli_query($polaczenie, $zapytanie);
+
+                while($dane = mysqli_fetch_array($wynik_zapytania))
+                {
+                    echo "<li>" . $dane["imie"] . " " . $dane["nazwisko"] . "</li>";
+                }
+                ?>
+            </ol>
         </section>
         <section id="blok-srodkowy">
             <h3>ul. Czytelnicza 25, Książkowice&nbsp;Wielkie</h3>
             <p><a href="mailto:sekretariat@biblioteka.pl">Napisz do nas</a></p>
             <img src="biblioteka.png" alt="książki" />
         </section>
-        <section class="blok-prawy" id="blok-prawy-gorny">
-            <h3>Dodaj czytelnika</h3>
-            <form action="biblioteka.php" method="post">
-                <label for="imie">imię:</label>
-                <input type="text" name="imie" id="imie">
-                <br />
+        <section id="blok-prawy">
+            <section id="blok-prawy-gorny">
+                <h3>Dodaj czytelnika</h3>
+                <form action="biblioteka.php" method="post">
+                    <label for="imie">imię:</label>
+                    <input type="text" name="imie" id="imie">
+                    <br />
 
-                <label for="nazwisko">nazwisko:</label>
-                <input type="text" name="nazwisko" id="nazwisko">
-                <br />
+                    <label for="nazwisko">nazwisko:</label>
+                    <input type="text" name="nazwisko" id="nazwisko">
+                    <br />
 
-                <label for="symbol">symbol:</label>
-                <input type="number" name="symbol" id="symbol">
-                <br />
+                    <label for="symbol">symbol:</label>
+                    <input type="number" name="symbol" id="symbol">
+                    <br />
 
-                <button type="submit">DODAJ</button>
-            </form>
-        </section>
-        <section class="blok-prawy" id="blok-prawy-dolny">
-            <!-- Skrypt 2 -->
+                    <button type="submit" name="submit">DODAJ</button>
+                </form>
+            </section>
+            <section id="blok-prawy-dolny">
+                <!-- Skrypt 2 -->
+                <?php
+                if(isset($_POST["submit"]))
+                {
+                    $imie = $_POST["imie"];
+                    $nazwisko = $_POST["nazwisko"];
+                    $symbol = $_POST["symbol"];
+
+                    echo "Czytelnik " . $imie . " " . $nazwisko . " został(a) dodany do bazy danych";
+
+                    $zapytanie = "INSERT INTO czytelnicy(imie, nazwisko, kod) VALUES('$imie', '$nazwisko', '$symbol');";
+
+                    mysqli_query($polaczenie, $zapytanie);
+                }
+                ?>
+            </section>
         </section>
     </main>
 
@@ -48,3 +80,7 @@
     </footer>
 </body>
 </html>
+
+<?php
+mysqli_close($polaczenie);
+?>
