31cbbaa2a4fea05ce68e55026865456268e541d5
Filip Madzia <filipmadzia33@gmail.com>
2024-11-30 13:50:00 +0100

    Skrypty

--- inf03_2023_06_01_sg/index.php
+++ inf03_2023_06_01_sg/index.php
@@ -1,3 +1,7 @@
+<?php
+$polaczenie = mysqli_connect('localhost', 'root', '', 'sklep');
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -15,12 +19,22 @@
             <h2>Taniej o 30%</h2>
             <ol>
                 <!-- Skrypt 1 -->
+                <?php
+                $zapytanie = "SELECT towary.nazwa FROM towary WHERE towary.promocja;";
+
+                $wynik = mysqli_query($polaczenie, $zapytanie);
+
+                while($dane = mysqli_fetch_array($wynik))
+                {
+                    echo "<li>" . $dane["nazwa"] . "</li>";
+                }
+                ?>
             </ol>
         </section>
         <section id="blok-srodkowy">
             <h2>Sprawdź cenę</h2>
             <form action="index.php" method="post">
-                <select name="towar">
+                <select name="nazwaTowaru">
                     <option value="Gumka do mazania">Gumka do mazania</option>
                     <option value="Cienkopis">Cienkopis</option>
                     <option value="Pisaki 60 szt.">Pisaki 60 szt.</option>
@@ -32,6 +46,23 @@
             
             <div id="wynik">
                 <!-- Skrypt 2 -->
+                <?php
+                if(isset($_POST["submit"]))
+                {
+                    $nazwaTowaru = $_POST["nazwaTowaru"];
+                    $zapytanie = "SELECT towary.cena FROM towary WHERE towary.nazwa = '$nazwaTowaru';";
+
+                    $wynik = mysqli_query($polaczenie, $zapytanie);
+
+                    $cena = mysqli_fetch_array($wynik)["cena"];
+
+                    $cenaPromocyjna = $cena * 0.7;
+
+                    echo "cena regularna: " . $cena;
+                    echo "<br>";
+                    echo "cena w promocji 30%: " . $cenaPromocyjna;
+                }
+                ?>
             </div>
         </section>
         <section id="blok-prawy">
@@ -46,3 +77,7 @@
     </footer>
 </body>
 </html>
+
+<?php
+mysqli_close($polaczenie);
+?>
