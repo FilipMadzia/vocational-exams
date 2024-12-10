a7269d3bb5c63bb80f2e546e9a0d95846cc19f3b
Filip Madzia <filipmadzia33@gmail.com>
2024-12-10 08:07:43 +0100

    Skrypty

--- ee09_2021_06_03_sg/video.php
+++ ee09_2021_06_03_sg/video.php
@@ -1,3 +1,16 @@
+<?php
+$connection = mysqli_connect('localhost', 'root', '', 'dane3');
+
+if(isset($_POST["submit"]) && isset($_POST["film-id"]))
+{
+    $filmId = $_POST["film-id"];
+
+    $query = "DELETE FROM produkty WHERE produkty.id = '$filmId';";
+
+    mysqli_query($connection, $query);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -30,10 +43,40 @@
         <section class="lists">
             <h3>Polecamy</h3>
             <!-- Skrypt 1 -->
+            <?php
+            $query = "SELECT produkty.id, produkty.nazwa, produkty.opis, produkty.zdjecie FROM produkty WHERE produkty.id IN (18, 22, 23, 25);";
+            $query_result = mysqli_query($connection, $query);
+
+            while($row = mysqli_fetch_array($query_result))
+            {
+                ?>
+                <div class="film-block">
+                    <h4><?php echo $row["id"] . ". " . $row["nazwa"];?></h4>
+                    <img src="<?php echo $row["zdjecie"];?>" alt="film" />
+                    <p><?php echo $row["opis"];?></p>
+                </div>
+                <?php
+            }
+            ?>
         </section>
         <section class="lists">
             <h3>Filmy fantastyczne</h3>
             <!-- Skrypt 2 -->
+            <?php
+            $query = "SELECT produkty.id, produkty.nazwa, produkty.opis, produkty.zdjecie FROM produkty WHERE produkty.Rodzaje_id = 12;";
+            $query_result = mysqli_query($connection, $query);
+
+            while($row = mysqli_fetch_array($query_result))
+            {
+                ?>
+                <div class="film-block">
+                    <h4><?php echo $row["id"] . ". " . $row["nazwa"];?></h4>
+                    <img src="<?php echo $row["zdjecie"];?>" alt="film" />
+                    <p><?php echo $row["opis"];?></p>
+                </div>
+                <?php
+            }
+            ?>
         </section>
     </main>
 
@@ -49,3 +92,7 @@
     </footer>
 </body>
 </html>
+
+<?php
+mysqli_close($connection);
+?>
