eae514a36ac1b4c3e15a0304e62523599be3ab21
Filip Madzia <filipmadzia33@gmail.com>
2025-01-08 20:56:25 +0100

    Skrypty

--- inf03_2024_06_11/peruwianka.php
+++ inf03_2024_06_11/peruwianka.php
@@ -1,3 +1,7 @@
+<?php
+$connection = mysqli_connect('localhost', 'root', '', 'hodowla');
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -19,15 +23,49 @@
             </section>
             <section id="lewy-dolny">
                 <img src="peruwianka.jpg" alt="Świnka morska rasy peruwianka">
+
                 <!-- Skrypt 2 -->
+                <?php
+                $query = mysqli_query($connection, 'SELECT DISTINCT swinki.data_ur, swinki.miot, rasy.rasa FROM swinki LEFT JOIN rasy ON swinki.rasy_id = rasy.id WHERE swinki.rasy_id = 1;');
+                $row = mysqli_fetch_array($query);
+                ?>
+
+                <h2>Rasa: <?php echo $row['rasa']; ?></h2>
+
+                <p>Data urodzenia: <?php echo $row['data_ur']; ?></p>
+                <p>Oznaczenie miotu: <?php echo $row['miot']; ?></p>
+
                 <hr>
                 <h2>Świnki w tym mioce</h2>
+
                 <!-- Skrypt 3 -->
+                <?php
+                $query = mysqli_query($connection, 'SELECT swinki.imie, swinki.cena, swinki.opis FROM swinki WHERE swinki.rasy_id = 1;');
+                while($row = mysqli_fetch_array($query))
+                {
+                ?>
+                    <h3><?php echo $row['imie']; ?> - <?php echo $row['cena']; ?> zł</h3>
+
+                    <p><?php echo $row['opis']; ?></p>
+                <?php
+                }
+                ?>
             </section>
         </section>
         <section id="prawy">
             <h3>Poznaj wszystkie rasy świnek morskich</h3>
-            <!-- Skrypt 1 -->
+
+            <ol>
+                <!-- Skrypt 1 -->
+                <?php
+                $query = mysqli_query($connection, 'SELECT rasy.rasa FROM rasy;');
+
+                while($row = mysqli_fetch_array($query))
+                {
+                    echo '<li>' . $row['rasa'] . '</li>';
+                }
+                ?>
+            </ol>
         </section>
     </main>
 
@@ -36,3 +74,7 @@
     </footer>
 </body>
 </html>
+
+<?php
+mysqli_close($connection);
+?>
