7ce6b05e9da7637a065dfd3b56e0d6720074df9f
Filip Madzia <filipmadzia33@gmail.com>
2025-01-08 17:46:26 +0100

    HTML + CSS

---
+++ inf03_2024_06_11/peruwianka.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Hodowla świnek morskich</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>Hodowla świnek morskich - zamów świnkowe maluszki</h1>
+    </header>
+
+    <main>
+        <section id="lewy">
+            <section id="lewy-gorny">
+                <a href="peruwianka.php">Rasa Peruwianka</a>
+                <a href="american.php">Rasa American</a>
+                <a href="crested.php">Rasa Crested</a>
+            </section>
+            <section id="lewy-dolny">
+                <img src="peruwianka.jpg" alt="Świnka morska rasy peruwianka">
+                <!-- Skrypt 2 -->
+                <hr>
+                <h2>Świnki w tym mioce</h2>
+                <!-- Skrypt 3 -->
+            </section>
+        </section>
+        <section id="prawy">
+            <h3>Poznaj wszystkie rasy świnek morskich</h3>
+            <!-- Skrypt 1 -->
+        </section>
+    </main>
+
+    <footer>
+        <p>Stronę wykonał: Filip Madzia</p>
+    </footer>
+</body>
+</html>
